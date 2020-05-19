d0d4bdf03aad41f042a06e79cf41453562ed33e9
nypawar <nypawar@gmail.com>
2020-05-19 09:15:25 +0530

    set

---
+++ multi.cpp
@@ -0,0 +1,19 @@
+/*
+ * multi.cpp
+ *
+ *  Created on: 19-May-2020
+ *      Author: nitin
+ */
+
+#include<iostream>
+#include<set>
+using namespace std;
+
+int main(void)
+{
+	set <int> s;
+
+	return 1;
+}
+
+
